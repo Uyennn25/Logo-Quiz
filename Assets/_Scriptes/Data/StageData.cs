b638a4c1b1b4ab293f5dae421a10ddc2d7cdc38f
Uyennn25 <tranhunguyen4@gmail.com>
2024-10-18 01:07:42 +0700

    Screen 1

---
+++ Assets/_Scriptes/Data/StageData.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Stage " , menuName = "Data / Stage Data")]
+
+public class StageData : ScriptableObject
+{
+    public int blankNumber;
+    public Sprite logo;
+    public string answer;
+    public List<string> alphabets; 
+}
